11e4cba9bd1ef0a285fbbfd9b63125aba4f72008
Princeyyyy <princemumo80@gmail.com>
2021-12-09 21:15:21 +0300

    merge head

--- connect.php
+++ connect.php
@@ -8,7 +8,6 @@ $servername = 'localhost';
 $username = 'root';
 $password = '';
 
-<<<<<<< HEAD
 if (!$conn) {
 
     die("Connection Failed: " .mysqli_connect_error());
@@ -16,14 +15,3 @@ if (!$conn) {
     echo("Connection Successful!");
 }
 ?>
-=======
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, 'startournament');
-
-// Check connection
-if(!$conn){
-    die("Database connection failed! " . mysqli_connect_error());
-}
-echo "Conected successfully!";
-?>
->>>>>>> dev
